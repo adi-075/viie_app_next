e6bef1610e66f8564977d584e3912d76da1da3e7
Aditya <37655818+adi-075@users.noreply.github.com>
2023-04-25 20:02:36 +0530

    Update about page imports and global css

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import about from "../../about.jpg";
+import about from "../../public/images/about.jpg";
 
 function page() {
   return (
@@ -13,7 +13,7 @@ function page() {
         <div className="bg-gradient-to-tl from-purple-900 to-green-900 h-96 w-full relative">
           <Image
             src={about}
-            alt=""
+            alt="About Image"
             className="w-full h-full object-cover absolute mix-blend-overlay"
           />
           {/* <img src='https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80' className='w-full h-full object-cover absolute mix-blend-overlay' /> */}
@@ -21,7 +21,7 @@ function page() {
             <h1 className="text-white text-6xl font-serif">About VIIE</h1>
           </div>
         </div>
-        
+
         <h1>Hello</h1>
       </div>
     </>
