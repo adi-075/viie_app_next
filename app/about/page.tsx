06486732c1189a332fd79c1278d81caa4c6477f2
Aditya <37655818+adi-075@users.noreply.github.com>
2023-04-27 10:33:53 +0530

    Update About

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,12 +18,12 @@ function page() {
             alt="About Image"
             width={2500}
             height={1660}
-            className="w-full h-full object-cover absolute mix-blend-overlay"
+            className="w-screen h-full object-cover absolute"
           />
           {/* <img src='https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80' className='w-full h-full object-cover absolute mix-blend-overlay' /> */}
-          <div className="py-40 flex justify-center items-center">
-            {/* <h1 className="text-white text-6xl">About VIIE</h1> */}
-          </div>
+          {/* <div className="py-40 flex justify-center items-center"> */}
+          {/* <h1 className="text-white text-6xl">About VIIE</h1> */}
+          {/* </div> */}
         </div>
 
         <h1 className="text-4xl text-center font-bold m-10">Our Journey</h1>
