2ab5ae22d2277850cd12305c62bd9676ea0331cc
Aditya <37655818+adi-075@users.noreply.github.com>
2023-04-27 10:52:19 +0530

    Update Favicon and Apple Touch Icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "./Navbar";
+import Head from "next/head";
 
 export default function RootLayout({
   children,
@@ -8,6 +9,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="shortcut icon" href="./favicon.ico" />
+        <link rel="apple-touch-icon" href="./apple-touch-icon.png" />
+      </Head>
       <body>
         <Navbar />
         {children}
