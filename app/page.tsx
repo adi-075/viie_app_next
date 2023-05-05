daaef3474eb4b6c0ecf5d4d5331982085333758f
Aditya <37655818+adi-075@users.noreply.github.com>
2023-05-05 20:32:00 +0530

    Fix Background Image for Mobile

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ const page = () => {
   return (
     <>
       <title>Home</title>
-      <div className="flex items-center justify-center h-screen mb-12 bg-fixed sm:bg-center sm:bg-cover custom-img-mb md:custom-img">
+      <div className="flex items-center justify-center h-screen mb-12 bg-fixed sm:bg-center bg-cover custom-img-mb md:custom-img">
         {/* Overlay  */}
         <div className="absolute top-0 left-0 right-0 bottom-0 z-[2]" />
         <div className="p-5 text-white z-[2] mt-[-10rem]">
