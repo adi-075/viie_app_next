21d4ed6a935147fb0879ab4d1afea0732359b80d
Aditya <37655818+adi-075@users.noreply.github.com>
2023-05-05 19:38:56 +0530

    Fix Bugs in Timeline

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Image from "next/image";
 import about from "../../public/images/Auditorium.png";
 import Timeline from "../timelineData";
-
 function page() {
   return (
     <>
@@ -11,28 +10,29 @@ function page() {
         <title>About VIIE</title>
       </head>
 
-      <body className="bg-gray-100 max-w-screen">
-        <div>
-          <div className="w-screen h-60 sm:h-96 relative">
-            <Image
-              priority
-              src={about}
-              alt="About Image"
-              width={2500}
-              height={1660}
-              className="w-screen h-full object-cover absolute"
-            />
-          </div>
-          <div className="md:flex justify-center items-center h-screen">
+      <div>
+        <div className="w-screen h-60 sm:h-96 relative">
+          <Image
+            priority
+            src={about}
+            alt="About Image"
+            width={2500}
+            height={1660}
+            className="w-screen h-full object-cover absolute"
+          />
+        </div>
+
+        <div className="md:flex justify-center items-center h-screen">
+          <div>
             <h1 className="text-4xl text-center font-bold py-5 px-2">
               Our Journey
             </h1>
-            <div className="mx-4 h-screen">
-              <Timeline />
-            </div>
+          </div>
+          <div className="mx-4 h-screen">
+            <Timeline />
           </div>
         </div>
-      </body>
+      </div>
     </>
   );
 }
--- app/timelineData.jsx
+++ app/timelineData.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import { Timeline, Text } from "@mantine/core";
@@ -19,6 +20,29 @@ const timeline = () => {
         bulletSize={12}
         align="left"
       >
+        <Timeline.Item title="Penn State University" bulletSize={12}>
+          <h1 className="font-bold font-serif text-red-950">2006</h1>
+          <Text color="dimmed" size="md">
+            Program: BS [IST]
+            <div>
+              <br />
+              <Image
+                src="/images/penn-state.png"
+                width={300}
+                height={300}
+                alt="Penn State"
+              />
+              <br />
+            </div>
+            We began our journey in 2006 with <br />
+            Pennsylvania State University introducing <br /> its first 2 + 2
+            credit transfer program in BS <br /> [Information Sciences and
+            Technology] to
+            <br /> incorporate a compelling blend of national
+            <br /> and international teaching methodologies
+          </Text>
+        </Timeline.Item>
+
         <Timeline.Item title="Penn State University" bulletSize={12}>
           <h1 className="font-bold font-serif text-red-950">2006</h1>
           <Text color="dimmed" size="md">
