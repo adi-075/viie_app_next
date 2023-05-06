3934a0b9705260f96820db422aeee9703cf84545
Aditya <37655818+adi-075@users.noreply.github.com>
2023-05-06 08:59:06 +0530

    Create Footer Component

---
+++ app/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      {/* TODO */}
+      {/* Add Mantine Buttons with Social Icons */}
+    </div>
+  );
+};
+
+export default Footer;
