461be6d884a25d5558b7e4f07b60607c082af6e2
Aditya <37655818+adi-075@users.noreply.github.com>
2023-05-08 09:24:27 +0530

    Add Blogs Page

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import { BackgroundImage, Center, Text, Box } from "@mantine/core";
+import { Button, Overlay, Image, AspectRatio } from "@mantine/core";
 
-const page = () => {
+export default function Demo() {
+  const [visible, setVisible] = useState(true);
   return (
-    <div>
-      <h1>Blogs</h1>
-    </div>
+    <Box maw={2500} h={1600}>
+      <AspectRatio ratio={5} mx="auto">
+        <BackgroundImage src="https://images.unsplash.com/photo-1544931170-3ca1337cce88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80">
+          <Center p="lg">
+            <Text color="#fff" weight={800} size={60}>
+              <br />
+              Blogs and Events
+              <br />
+              <br />
+            </Text>
+          </Center>
+        </BackgroundImage>
+      </AspectRatio>
+    </Box>
   );
-};
-
-export default page;
+}
