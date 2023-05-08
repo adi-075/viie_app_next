c5fb11b8095b44f4f04fa0620af07b07f6817839
Aditya <37655818+adi-075@users.noreply.github.com>
2023-05-08 08:46:45 +0530

    Change Fonts in Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { homeData } from "./homeData";
 import { Mandali } from "next/font/google";
 import Events from "./Events";
 import bgmb from "../bg-image-mb.jpg";
+import { Source_Sans_Pro } from "next/font/google";
+const source = Source_Sans_Pro({ subsets: ["latin"], weight: "400" });
 // import "./homepage.css";
 
 const page = () => {
@@ -23,31 +25,33 @@ const page = () => {
       </div>
       <div className="flex flex-col items-center justify-center">
         <h2 className="text-5xl font-bold text-center mb-5">This is VIIE</h2>
-        <p className="text-center p-3 sm:p-0">
-          Vidyalankar has collaborated with top universities in the USA, CANADA,
-          UK, Australia, and New Zealand and has developed some unique programs{" "}
-          <br />
-          at the undergraduate and postgraduate levels. Our programs equip the
-          students with the right toolset, suitable skillset and, more
-          importantly, the right <br /> mindset. Ever since they were started in
-          2006, they have been well received by the students <br />, their
-          parents and by the industry.
-        </p>
-        {/* <button className="mx-auto bg-red-600 p-1">More about VIIE</button> */}
+        <div className="text-center text-xl md:text-2xl p-3 sm:p-0 break-normal max-w-[1000px]">
+          <p className={source.className}>
+            Vidyalankar has collaborated with top universities in the USA,
+            CANADA, UK, Australia, and New Zealand and has developed some unique
+            programs at the undergraduate and postgraduate levels. Our programs
+            equip the students with the right toolset, suitable skillset and,
+            more importantly, the right mindset. Ever since they were started in
+            2006, they have been well received by the students, their parents
+            and by the industry.
+          </p>
+        </div>
         <Link href="/about">
-          <button className="mt-5 bg-[#9c0e0e] text-white font-bold py-1 px-4 rounded mb-10">
+          <button className="mt-5 bg-[#9c0e0e] text-white text-xl font-bold py-4 px-12 rounded mb-10">
             More About VIIE
           </button>
         </Link>
       </div>
-      <div className="flex flex-col justify-center bg-[#f4f4f4]">
+      <div className="flex flex-col justify-center items-center bg-[#f4f4f4]">
         <h1 className="mx-auto text-4xl font-bold mb-5 my-10">Academics</h1>
-        <p className="mb-8 px-6 text-center">
-          The main objective of education is to equip students with the
-          necessary skills <br /> and knowledge to actively participate as
-          responsible members of society and make significant <br />{" "}
-          contributions.
-        </p>
+        <div>
+          <p className="{source.className} text-center text-xl md:text-2xl break-normal max-w-[1000px]">
+            The main objective of education is to equip students with the
+            necessary skills and knowledge to actively participate as
+            responsible members of society and make significant contributions.
+          </p>
+          <br />
+        </div>
         <div className="event-list grid grid-fow-row md:grid-flow-col gap-10 md:gap-14 justify-center px-2">
           <div className="hover:scale-125 transition-all duration-500 cursor-pointer">
             <Image
