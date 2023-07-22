a38d7fbe70edf8f7b6f7223a63ff9a249fb45dfc
Aditya <37655818+adi-075@users.noreply.github.com>
2023-07-22 14:11:16 +0530

    Add Canadian Universities

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -3,16 +3,17 @@ import Image from "next/image";
 
 function Card(props) {
   return (
-    <div>
+    <div className="xl:hidden">
+      <h1 class="font-bold text-4xl text-center mt-7">US UNIVERSITIES</h1>
       <div className="flex flex-col space-y-10 justify-center items-center h-full mx-7 mt-10 relative xl:hidden">
         <a href="/rutgers">
           <div className="align-middle items-center text-center relative flex justify-center">
             <img
-              src="/csu.jpg"
+              src="/rutgers.jpg"
               alt="Rutgers"
               width={500}
               height={500}
-              className="rounded-2xl"
+              className="rounded-2xl shadow-xl drop-shadow-lg"
             />
             <h1 className="text-2xl absolute z-10 font-sans font-bold m-2 text-white">
               RUTGERS UNIVERSITY
@@ -26,7 +27,7 @@ function Card(props) {
               alt="Rutgers"
               width={500}
               height={500}
-              className="rounded-2xl"
+              className="rounded-2xl shadow-xl drop-shadow-lg"
             />
             <h1 className="text-2xl absolute z-10 font-sans font-bold m-2 text-white">
               PENN STATE UNIVERSITY
@@ -40,38 +41,43 @@ function Card(props) {
               alt="Rutgers"
               width={500}
               height={500}
-              className="rounded-2xl"
+              className="rounded-2xl shadow-xl drop-shadow-lg"
             />
             <h1 className="text-2xl absolute z-10 font-sans font-bold m-2 text-white">
               CALIFORNIA STATE UNIVERSITY
             </h1>
           </div>
         </a>
-        <a href="/csu">
+      </div>
+      <h1 class="font-bold text-4xl text-center mt-10">
+        CANADIAN UNIVERSITIES
+      </h1>
+      <div className="flex flex-col space-y-10 justify-center items-center h-full mx-7 mt-10 relative xl:hidden">
+        <a href="/ul">
           <div className="align-middle items-center text-center relative flex justify-center">
             <img
-              src="/csu.jpg"
-              alt="Rutgers"
+              src="/ul.jpg"
+              alt="University of lethbridge"
               width={500}
               height={500}
-              className="rounded-2xl"
+              className="rounded-2xl shadow-xl drop-shadow-lg"
             />
             <h1 className="text-2xl absolute z-10 font-sans font-bold m-2 text-white">
-              NULL
+              UNIVERSITY OF LETHBRIDGE
             </h1>
           </div>
         </a>
-        <a href="/csu">
+        <a href="/uf">
           <div className="align-middle items-center text-center relative flex justify-center">
             <img
-              src="/csu.jpg"
-              alt="Rutgers"
+              src="/uf.jpg"
+              alt="University of Fraser Valley"
               width={500}
               height={500}
-              className="rounded-2xl"
+              className="rounded-2xl shadow-xl drop-shadow-lg"
             />
             <h1 className="text-2xl absolute z-10 font-sans font-bold m-2 text-white">
-              NULL
+              UNIVERSITY OF LETHBRIDGE
             </h1>
           </div>
         </a>
